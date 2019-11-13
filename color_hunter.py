82fc970fcc4388f897e8f833ad497c04c2121b9f
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2019-11-13 13:20:52 -0500

    consolidated code

--- color_hunter.py
+++ color_hunter.py
@@ -9,13 +9,12 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from time import sleep
 
-# setup page scraper
+# ---- scrape colors from internet -------------------------------------------
 chrome_options = Options()
 chrome_options.add_argument("--dns-prefetch-disable")
 chrome_options.add_argument("--no-proxy-server")
 bot = webdriver.Chrome(chrome_options=chrome_options)
 
-themes = {} # store collected themes
 url = "https://colorhunt.co/palettes"
 
 # navigate to the target url
@@ -25,17 +24,18 @@ bot.get(url)
 last_height = bot.execute_script("return document.body.scrollHeight")
 while True:
     bot.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(2) # may need to adjust based on lag and connection speed
+    sleep(1.25) # may need to adjust based on lag and connection speed
     new_height = bot.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
         break    
     last_height = new_height
 
 # extract color palettes from html source
-soup = BeautifulSoup(bot.page_source, 'lxml')
+soup = BeautifulSoup(bot.page_source)
 pal = soup.find_all("div",{"class":"palette"})
 
 # iterate through each color palette and save to themes
+themes = {}
 for row in pal:
     color = row.find_all('div')
     c1 = color[3].text
@@ -43,14 +43,38 @@ for row in pal:
     c3 = color[1].text
     c4 = color[1].text
     theme_name = color[0].a['href'].split('/').pop()
-    themes[theme_name] = (c1, c2, c3, c4)
+    if theme_name:
+        themes[theme_name] = (c1, c2, c3, c4)
 
-bot.close()
+print(themes)
 
-# write dictionary to python file
-with open('colors.py','w') as f:
-    f.write('color_hunt = {\n')
+# ---- get contrasting text color --------------------------------------------
+# adapted to python from: https://24ways.org/2010/calculating-color-contrast/
+
+def get_contrast_yiq(hex_color):
+    """ get contrasting black or white text color for a hex color """
+    # convert hex to rbg
+    r = int(hex_color[1:3], 16)
+    g = int(hex_color[3:5], 16)
+    b = int(hex_color[5:7], 16)
+    # scale according to visual impact compare to middle
+    yiq = ((r*299)+(g*587)+(b*114))//1000
+    if yiq >= 128:
+        return '#000000' # black
+    else:
+        return '#FFFFFF' # white
+
+# add a constrasting color to each theme in the color_hunt dictionary
+for key, val in themes.items():
+    prime = val[0]
+    text_color = get_contrast_yiq(prime)
+    themes[key] = val + (text_color,)
+
+# export new colors with contrasting text color
+with open('themes.py','w') as f:
+    f.write('themes = {\n')
     for key, val in themes.items():
         f.write(f"\t'{key}': {val},\n")
     f.write('}')
 
+print('Scraping complete!')
